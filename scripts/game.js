10f6a89f83a8f3a3a782e655bd9366cbefd7187c
diegoami <diego.amicabile@gmail.com>
2019-12-19 15:58:23 +0100

    Game over added

--- scripts/game.js
+++ scripts/game.js
@@ -3,6 +3,8 @@ BALLOON_FREQUENCY = 0.1;
 BALLOON_SPEED = 5;
 MAX_BALLOONS = 200;
 RATIO_SIZE = 1;
+MAX_LOST_BALLOONS = 5;
+ESCAPE_COORDS = -10;
 
 var balloonConstructor = function(xcoord, ycoord, size, color, xmax, speed) {
     var that;
@@ -29,7 +31,7 @@ var balloonConstructor = function(xcoord, ycoord, size, color, xmax, speed) {
 
 
     that.draw = function(canvas) {
-        if (this.ycoord > -100) {
+        if (this.ycoord > ESCAPE_COORDS) {
             var balloon = new CANVASBALLOON.Balloon('balloon_canvas', this.xcoord, this.ycoord, this.size, this.color);
             balloon.draw();
         }
@@ -58,21 +60,17 @@ function getRandomCssColor() {
 Game.do_click = function() {
 
     var that = this;
+    $('#balloon_canvas').unbind('click');
     $('#balloon_canvas').click( function(event) {
         var sender = event.target;
         var bounding_rect = sender.getBoundingClientRect();
         var canvas_ratio_x = bounding_rect.width / sender.width;
         var canvas_ratio_y = bounding_rect.height / sender.height;
-
-  //      var x = event.clientX / canvas_ratio_x,
-  //          y = event.clientY / canvas_ratio_y;
-
         var x = event.clientX / canvas_ratio_x,
             y = event.clientY / canvas_ratio_y;
-        var caught
+        var balls_caught = undefined;
         if (x && y) {
-            for (var i = that.balloons.length - 1; i >= 0; i--) {
-
+            for (var i = that.balloons.length - 1; i >= 0 && !balls_caught; i--) {
                 if (that.balloons[i].collision(x , y)) {
                     that.balloons.splice(i, 1);
                     that.balloons_caught++;
@@ -83,9 +81,38 @@ Game.do_click = function() {
     });
 }
 
+Game.restart = function() {
+    this.do_click();
+    if (this.tick_interval) {
+        clearInterval(this.tick_interval);
+    }
+    this.isrestart = false;
+    this.clear();
+    this.balloons = [];
+    this.balloons_caught = 0;
+    this.lostBalloons = 0;
+
+    this.fontSize = 30 * this.ratio ;
+    this.ctx.font = (this.fontSize|30) + "px Verdana";
+
+    // Create gradient
+    var gradient = this.ctx.createLinearGradient(0, 0, this.canvas.width, 0);
+    for (var i = 0; i < 1; i += 0.05) {
+        gradient.addColorStop(i, getRandomCssColor());
+    }
+    this.ctx.fillStyle = gradient;
+    this.ctx.fillText("Stop the balloons, before it is too late !!",this.canvas.width * 0.1,this.canvas.height * 0.15);
+    that = this;
+
+    setTimeout(function() {
+        that.start = Date.now();
+        that.tick_interval = setInterval(Game.run, 1000 / Game.fps);
+    }, 2000);
+
+
+}
+
 Game.init = function() {
-
-
     this.canvas = $('#balloon_canvas')[0];
     //this.canvasbackup = $('#balloon_canvas').clone(true)[0];
     this.canvas.width = window.innerWidth;
@@ -93,37 +120,11 @@ Game.init = function() {
     this.ctx = this.canvas.getContext('2d');
 
     this.ratio = this.old_ratio = this.canvas.width / 1000;
-    this.event_ratio = 1
 
     this.div = $('#div_canvas');
-    this.balloons = [];
-    this.balloons_caught = 0;
-    this.lostBalloons = 0;
-
-    this.do_click();
-
-    this.fontSize = 30 * this.ratio ;
-    this.ctx.font = (this.fontSize|30) + "px Verdana";
 
 
-    // Create gradient
-    var gradient = this.ctx.createLinearGradient(0, 0, this.canvas.width, 0);
-
-    for (var i = 0; i < 1; i += 0.05) {
-
-        gradient.addColorStop(i, getRandomCssColor());
-
-    }
-    this.ctx.fillStyle = gradient;
-    this.ctx.fillText("Stop the balloons, before it is too late !!",this.canvas.width * 0.1,this.canvas.height * 0.15);
-    that = this;
-
-    setTimeout(function() {
-        Game.clear();
-        that.start = Date.now();
-        setInterval(Game.run, 1000 / Game.fps);
-    }, 2000);
-
+    this.restart();
 }
 
 function getRandomItem(set) {
@@ -132,6 +133,26 @@ function getRandomItem(set) {
 }
 
 Game.gameover = function() {
+    if (!this.isrestart) {
+        this.end_time = this.time_to_show;
+        var gradient = this.ctx.createLinearGradient(0, 0, this.canvas.width, 0);
+        for (var i = 0; i < 1; i += 0.05) {
+            gradient.addColorStop(i, getRandomCssColor());
+        }
+        this.ctx.fillStyle = gradient;
+        that = this;
+        this.isrestart = true;
+        setTimeout(function () {
+            $('#balloon_canvas').click( function(event) {
+                that.restart();
+            });
+        }, 6000);
+    }
+    if (this.end_time) {
+        this.ctx.fillText("Game Over. Score: " + this.balloons_caught + ", Time: " + this.end_time, this.canvas.width * 0.2, this.canvas.height * 0.2);
+        this.ctx.fillText("Click to restart", this.canvas.width * 0.35, this.canvas.height * 0.35);
+
+    }
 
 }
 
@@ -156,12 +177,10 @@ Game.randomBalloon = function() {
 
 Game.tick = function() {
 
-    for (var i = 0; i < this.balloons.length; i++) {
-        if (this.balloons[i].ycoord < -50) {
+    for (var i = this.balloons.length-1; i >= 0; i--) {
+        if (this.balloons[i].ycoord <= ESCAPE_COORDS) {
             this.balloons.splice(i,1);
-            i--;
             this.lostBalloons++;
-            $('#balloon_counter').html(this.lostBalloons);
         }
     }
 
@@ -177,6 +196,10 @@ Game.tick = function() {
     for (var i in this.balloons) {
         this.balloons[i].tick();
     }
+    if (this.lostBalloons >= MAX_LOST_BALLOONS) {
+        this.gameover();
+    }
+
 
 }
 
@@ -186,8 +209,8 @@ Game.draw = function() {
     }
     if (this.ctx) {
         this.ctx.fillText(this.balloons_caught + "/" +this.lostBalloons, this.canvas.width * 0.1, this.canvas.height * 0.1)
-        var time_to_show = ((Date.now() - this.start) / 1000).toFixed(2);
-        this.ctx.fillText(time_to_show, this.canvas.width * 0.8, this.canvas.height * 0.1)
+        this.time_to_show = ((Date.now() - this.start) / 1000).toFixed(2);
+        this.ctx.fillText(this.time_to_show, this.canvas.width * 0.8, this.canvas.height * 0.1)
     }
 
 };

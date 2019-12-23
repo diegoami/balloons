e9e7e9a6ab05a9e035e7eeec226398bbe88f6fc3
diegoami <diego.amicabile@gmail.com>
2019-12-23 18:38:06 +0100

    Manage gradients better

--- scripts/game.js
+++ scripts/game.js
@@ -15,8 +15,13 @@ const START_TEXT_BEGIN_X = 0.05;
 const START_TEXT_INTRO_X = 0.05;
 const START_TEXT_INTRO_Y = 0.05;
 const START_TEXT_BEGIN_Y = 0.2;
+const HIGH_SCORE_BEGIN_X = 0.28;
+const HIGH_SCORE_BEGIN_Y = 0.37;
+
 
 const DIFF_0 = "Select ", DIFF_1 = "E: Easy", DIFF_2 = "S: Standard", DIFF_3 = "H: Hard", DIFF_4 = "V: VHard";
+const HIGH_SCORES_TEXT = "High Scores - ";
+const HIGH_SCORES_LENGTH = HIGH_SCORES_TEXT.length;
 const DIFF_TOTAL = DIFF_0 + DIFF_1 + ", " + DIFF_2 + ", " + DIFF_3 + ", " + DIFF_4;
 const DIFF_OFFSET_1 = DIFF_0.length, DIFF_LENGTH_1 =  DIFF_1.length;
 const DIFF_OFFSET_2 = DIFF_OFFSET_1 + DIFF_LENGTH_1 + 2, DIFF_LENGTH_2 = DIFF_2.length
@@ -38,7 +43,17 @@ var Game = { };
 Game.fps = 30;
 
 
+Game.updateGradient = function () {
 
+    if (!this.is_gradient) {
+        var gradient = this.ctx.createLinearGradient(0, 0, this.canvas.width, 0);
+        for (var i = 0; i < 1; i += 0.05) {
+            gradient.addColorStop(i, getRandomCssColor());
+        }
+        this.ctx.fillStyle = gradient;
+        this.is_gradient = 1;
+    }
+}
 
 Game.do_click = function() {
     $(document).unbind();
@@ -68,6 +83,7 @@ Game.do_click = function() {
 
 
 Game.restart = function(diff_level) {
+    Cookies.set("diff_level", diff_level);
     if (diff_level == 'E' )  {
         MAX_LOST_BALLOONS = 15;
         RATIO_DECREASE = 2000;
@@ -89,7 +105,6 @@ Game.restart = function(diff_level) {
         SPEED_INCREASE =  80;
         DIFF_LEVEL= 'VHARD';
     }
-
     this.diff_level = diff_level;
     this.do_click();
     if (this.tick_interval) {
@@ -112,16 +127,21 @@ Game.restart = function(diff_level) {
 
 Game.init = function() {
     that = this;
-    $.getJSON('http://gd.geobytes.com/GetCityDetails?callback=?', function(data) {
-        var location = data["geobyteslocationcode"];
-        that.name = prompt("Please enter your name", location);
-    });
-    var search_substr = window.location.search.substr(1);
-    if (search_substr) {
-        MAX_LOST_BALLOONS =  parseInt(search_substr) || MAX_LOST_BALLOONS;
+    this.name = Cookies.get("name");
+    if (this.name) {
+        this.name = prompt("Please enter your name", this.name);
+        Cookies.set('name', this.name) // => 'value'
+    } else {
+        $.getJSON('http://gd.geobytes.com/GetCityDetails?callback=?', function(data) {
+            var location = data["geobyteslocationcode"];
+            that.name = prompt("Please enter your name", location);
+            Cookies.set('name', that.name) // => 'value'
+
+        });
+
     }
+
     this.canvas = $('#balloon_canvas')[0];
-    //this.canvasbackup = $('#balloon_canvas').clone(true)[0];
     this.canvas.width = window.innerWidth;
     this.canvas.height = window.innerHeight;
     this.ctx = this.canvas.getContext('2d');
@@ -130,15 +150,14 @@ Game.init = function() {
 
     this.fontSize = DEFAULT_FONT_SIZE * this.ratio ;
     this.ctx.font = (this.fontSize | DEFAULT_FONT_SIZE) + "px Verdana";
-
-    // Create gradient
-    var gradient = this.ctx.createLinearGradient(0, 0, this.canvas.width, 0);
-    for (var i = 0; i < 1; i += 0.05) {
-        gradient.addColorStop(i, getRandomCssColor());
-    }
-    this.ctx.fillStyle = gradient;
+    this.is_gradient = 0;
+    this.updateGradient();
     this.ctx.fillText("Stop the balloons, before it is too late !!",this.canvas.width * START_TEXT_INTRO_Y,this.canvas.height * START_TEXT_INTRO_X);
-    this.diff_level = "E";
+
+    this.diff_level = Cookies.get("diff_level");
+    if (!this.diff_level) {
+        this.diff_level = "S";
+    }
 
     this.ctx.fillText(DIFF_TOTAL,this.canvas.width * START_TEXT_BEGIN_X,this.canvas.height * START_TEXT_BEGIN_Y);
     this.getScores();
@@ -148,7 +167,7 @@ Game.init = function() {
 Game.setDifficulty = function() {
 
     let bwidth =  this.ctx.measureText(DIFFICULTY_CHOICE).width;
-    let bheight = this.ctx.measureText('M').width;
+    let bheight = this.ctx.measureText('M').width*1.2;
 
 
     that = this;
@@ -163,6 +182,12 @@ Game.setDifficulty = function() {
             y = event.clientY / canvas_ratio_y;
         var tcv = that.canvas.width * START_TEXT_BEGIN_X ;
         var tch = that.canvas.height * START_TEXT_BEGIN_Y - bheight / 2;
+
+        var tsv = that.canvas.width * HIGH_SCORE_BEGIN_X ;
+        var tsh = that.canvas.height * HIGH_SCORE_BEGIN_Y - bheight / 2;
+
+
+
         if ((y >= tch) && (y <= tch + bheight)) {
             if ((x >= tcv + x_unit * (DIFF_OFFSET_1-0.5) )  && (x <= tcv + x_unit * (DIFF_OFFSET_1 + DIFF_LENGTH_1)) ) {
                 that.restart("E");
@@ -173,12 +198,16 @@ Game.setDifficulty = function() {
             } else if ((x >= tcv + x_unit * (DIFF_OFFSET_4 -0.5))  && (x <= tcv + x_unit * (DIFF_OFFSET_4 + DIFF_LENGTH_4)) ) {
                 that.restart("V");
             }
+        } else if ((y >= tsh) && (y <= tsh + bheight)) {
+            if ((x >= tsv  )  && (x <= tsv + x_unit * ( HIGH_SCORES_LENGTH + 4)) ) {
+                that.restart(that.diff_level);
+            }
         }
     });
 
     $(document).keydown( function(event) {
         if (event.keyCode == 32 || event.keyCode == 13) {
-            that.restart();
+            that.restart(that.diff_level);
         } else if (event.key == 'e' || event.key == 'E') {
             that.restart("E");
         } else if (event.key == 's' || event.key == 'S') {
@@ -196,7 +225,10 @@ Game.fillscore = function(data) {
     const SCORE_Y = [0.5, 0.65, 0.8];
     that = this;
     if (data) {
-        that.ctx.fillText("HIGH SCORES - "+that.diff_level, that.canvas.width * 0.28, that.canvas.height * 0.37)
+
+
+
+        that.ctx.fillText(HIGH_SCORES_TEXT + that.diff_level, that.canvas.width * HIGH_SCORE_BEGIN_X, that.canvas.height * HIGH_SCORE_BEGIN_Y)
 
         for (var i = 0; i < 3; i++) {
             if (data.length > i) {
@@ -229,7 +261,6 @@ Game.addscore = function(score) {
     var url = SCORE_URL + this.diff_level.toLowerCase();
     that = this;
     $.post(url, {"score" :  score , "name" : that.name} , function() {
-        that.getScores();
     },  "json");
 }
 
@@ -238,11 +269,7 @@ Game.gameover = function() {
         this.addscore(this.balloons_caught);
         $('#balloon_canvas').unbind('click');
         this.end_time = this.time_to_show;
-        var gradient = this.ctx.createLinearGradient(0, 0, this.canvas.width, 0);
-        for (var i = 0; i < 1; i += 0.05) {
-            gradient.addColorStop(i, getRandomCssColor());
-        }
-        this.ctx.fillStyle = gradient;
+;
         that = this;
         this.isrestart = true;
         setTimeout(function () {
@@ -252,6 +279,9 @@ Game.gameover = function() {
         }, 5000);
     }
     if (this.end_time) {
+        if (this.balloons.length == 0) {
+            this.updateGradient();
+        }
         this.ctx.fillText("Game Over. Score: " + this.balloons_caught + ", Time: " + this.end_time, this.canvas.width * START_TEXT_INTRO_X, this.canvas.height * START_TEXT_INTRO_Y);
         this.ctx.fillText(DIFF_TOTAL,this.canvas.width * START_TEXT_BEGIN_X,this.canvas.height * START_TEXT_BEGIN_Y);
         if (this.showscores) {
@@ -305,6 +335,9 @@ Game.tick = function() {
 Game.draw = function() {
     for (var i in this.balloons) {
         this.balloons[i].draw();
+    }
+    if (this.balloons.length > 0) {
+        this.is_gradient = 0;
     }
     if (this.ctx && !this.isrestart) {
         this.ctx.fillText(this.balloons_caught + "/" +this.lostBalloons, this.canvas.width * 0.1, this.canvas.height * 0.1);
